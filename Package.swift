cedeaccce7d47adc77df53624da92162d6c6ad46
Christoffer Buusmann <foffer@gmail.com>
2020-03-12 07:50:39 +0000

    Bumped swift tools version

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.0
 
 import PackageDescription
 
@@ -9,7 +9,6 @@ pkg.products = [
     .library(name: "ProcedureKitCloud", targets: ["ProcedureKitCloud"]),
     .library(name: "ProcedureKitCoreData", targets: ["ProcedureKitCoreData"]),
     .library(name: "ProcedureKitLocation", targets: ["ProcedureKitLocation"]),
-    .library(name: "ProcedureKitMac", targets: ["ProcedureKitMac"]),
     .library(name: "ProcedureKitNetwork", targets: ["ProcedureKitNetwork"]),
     .library(name: "TestingProcedureKit", targets: ["TestingProcedureKit"])
 ]
@@ -19,7 +18,6 @@ pkg.targets = [
     .target(name: "ProcedureKitCloud", dependencies: ["ProcedureKit"]),
     .target(name: "ProcedureKitCoreData", dependencies: ["ProcedureKit"]),    
     .target(name: "ProcedureKitLocation", dependencies: ["ProcedureKit"]),
-    .target(name: "ProcedureKitMac", dependencies: ["ProcedureKit"]),
     .target(name: "ProcedureKitNetwork", dependencies: ["ProcedureKit"]),
     .target(name: "TestingProcedureKit", dependencies: ["ProcedureKit"]),
     .testTarget(name: "ProcedureKitTests", dependencies: ["ProcedureKit", "TestingProcedureKit"]),
@@ -27,6 +25,5 @@ pkg.targets = [
     .testTarget(name: "ProcedureKitCloudTests", dependencies: ["ProcedureKitCloud", "TestingProcedureKit"]),
     .testTarget(name: "ProcedureKitCoreDataTests", dependencies: ["ProcedureKitCoreData", "TestingProcedureKit"]),    
     .testTarget(name: "ProcedureKitLocationTests", dependencies: ["ProcedureKitLocation", "TestingProcedureKit"]),
-    .testTarget(name: "ProcedureKitMacTests", dependencies: ["ProcedureKitMac", "TestingProcedureKit"]),
     .testTarget(name: "ProcedureKitNetworkTests", dependencies: ["ProcedureKitNetwork", "TestingProcedureKit"]),
 ]
